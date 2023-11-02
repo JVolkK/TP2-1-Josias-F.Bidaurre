8c0b205020878203787bfe2d43f5dd58300ffe84
JVolkK <122408877+JVolkK@users.noreply.github.com>
2023-11-02 19:23:45 -0300

    Agregar archivos de proyecto.

---
+++ TP2-1-Josias F.Bidaurre/Cuenta.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace TP2_1_Josias_F.Bidaurre
+{
+    internal class Cuenta
+    {
+        private string _titular;
+        private decimal _cantidad;
+
+        public string Titular
+        {
+            get { return _titular; }
+            set { _titular = value; }
+        }
+
+        public decimal Cantidad
+        {
+            get { return _cantidad; }
+            set { _cantidad = value; }
+        }
+
+        public Cuenta(string titular, decimal cantidad)
+        {
+            _titular = titular;
+            _cantidad = cantidad;
+        }
+
+        public Cuenta(string titular) : this(titular, 0)
+        {
+            // Constructor que llama al otro constructor con 0 como cantidad por defecto.
+        }
+
+        public void Deposito(decimal cantidad)
+        {
+            if (cantidad > 0)
+            {
+                _cantidad += cantidad;
+            }
+            // No se hace nada si la cantidad es negativa.
+        }
+
+        public void Retiro(decimal cantidad)
+        {
+            if (cantidad <= 0)
+            {
+                // No se hace nada si la cantidad es negativa o cero.
+                return;
+            }
+
+            if (cantidad > _cantidad)
+            {
+                _cantidad = 0;
+            }
+            else
+            {
+                _cantidad -= cantidad;
+            }
+        }
+    }
+}
---
+++ TP2-1-Josias F.Bidaurre/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TP2_1_Josias_F.Bidaurre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta cuenta1 = new Cuenta("Patricio Cainzo");
+            Cuenta cuenta2 = new Cuenta("Mauro Bringas", 2000);
+            Console.WriteLine("La cuenta pertenece a " + cuenta1.Titular + " y contiene: " + cuenta1.Cantidad);
+            Console.WriteLine("La cuenta pertenece a " + cuenta2.Titular + " y contiene: " + cuenta2.Cantidad);
+
+            // Ingresa cantidad positiva y se suma a la cantidad existente
+            cuenta1.Deposito(4000);
+            Console.WriteLine("Se hizo un depósito. La cantidad actual es: " + cuenta1.Cantidad);
+
+            // Ingresa cantidad negativa y la aplicación no hace nada
+            cuenta1.Deposito(-4000);
+            Console.WriteLine("Se intentó depositar una cantidad negativa. La cantidad actual es: " + cuenta1.Cantidad);
+
+            // Retira una cantidad positiva y se resta a la cantidad existente
+            cuenta1.Retiro(500);
+            Console.WriteLine("Se hizo un retiro. La cantidad actual es: " + cuenta1.Cantidad);
+
+            // Ingresa una cantidad negativa y no se hace nada
+            cuenta1.Retiro(-500);
+            Console.WriteLine("Se intentó retirar una cantidad negativa. La cantidad actual es: " + cuenta1.Cantidad);
+
+            // Si la cantidad a retirar es mayor a la cantidad existente, la cuenta queda en 0 como pide el práctico
+            cuenta1.Retiro(7000);
+            Console.WriteLine("Se intentó retirar una cantidad mayor al saldo. La cantidad actual es: " + cuenta1.Cantidad);
+
+            Console.ReadKey();
+        }
+    }
+}
